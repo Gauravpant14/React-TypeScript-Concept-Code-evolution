2aceb26903961e0a164a8bf4009b112e24b986fc
gaurav <aadeezcity@gmail.com>
2021-09-14 16:42:24 +0530

    event props #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Award } from './components/Award';
+import Button from './components/Button';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
 import { Person } from './components/Person';
@@ -7,43 +8,12 @@ import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
 
 function App() {
-  const personName ={
-    first:'john',
-    middleName:'chandra',
-    last:'cena',
-  }
-  const nameList = [
-    {
-      first: 'Oscar',
-      last: 'David'
-    },
-    {
-      first: 'john',
-      last: 'abbacus'
-    },
-    {
-      first: 'Amit',
-      last: 'Pandey'
-    }
-  ]
-
+const handleClick = () => {
+console.log('Button Clicked');
+}
   return (
     <div className="App">
-     <Greet name='Gaurav' messageCount={20} isLoggedIn={false}/>
-     <Person name={personName} />
-     <PersonList names={nameList}/>
-     <Status status="Loading" />
-     <Heading>
-       Exaple of component children Props passing
-     </Heading>
-     <Award>
-     <Heading>
-      Best Bollywood actor award goes to : Nobody.
-     </Heading>
-     <Heading>
-     This is the example of passing react component as a children props. 
-     </Heading>
-     </Award>
+      <Button handleClick={() => handleClick()}/>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+    handleClick: () => void
+
+}
+
+function Button(props: ButtonProps) {
+    return (
+        <button onClick={props.handleClick}>
+            Click
+        </button>
+    )
+}
+
+export default Button
